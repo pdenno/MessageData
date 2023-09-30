3cc7dffd8011faf66478a3f0ad529d5b2bd00b12
Peter Denno <podenno@gmail.com>
2023-09-30 19:14:31 -0400

    Shutdown sync

--- src/message_data/core.clj
+++ src/message_data/core.clj
@@ -1,12 +1,14 @@
 (ns message-data.core
   (:require
-   [clojure.java.io :as io]
+   [clojure.java.io      :as io]
+   [clojure.pprint       :refer [cl-format pprint]]
    [datahike.api         :as d]
    [datahike.pull-api    :as dp]
    [message-data.util    :as util :refer [connect-atm db-for! get-schema list-schemas register-db resolve-db-id]] ; For mount
    [mount.core           :as mount :refer [defstate]]
    [taoensso.timbre      :as log]))
 
+(def diag (atom nil))
 (def db-cfg-atm "Configuration map used for connecting to the db. It is set in core."  (atom nil))
 (def base-dir "The base directory of the databases. Can't be set at compile time in Docker." nil)
 (def db-dir "The directory containing schema DBs. Can't be set at compile time in Docker." nil)
@@ -23,19 +25,54 @@
       distinct
       (sort-by :elem/name)))
 
-(defn get-schema-names
-  "Get the element_names"
-  []
-  (->> (d/q '[:find [?name ...]
-              :where
-              [_ :schema_name ?name]]
-               @(connect-atm))
-       sort))
+(def sought-sdo? nil)
+(def sought-type? nil)
+(defn get-schema-names ; There are 226 schema meeting the default requirements.
+  "Get the names of schema that meet requirements:
+    - sdos  : a set, defaults to #{:oagi :oasis :qif}.
+    - types : a set, defaults to #{::generic_messageSchema :cct_bie :generic_xsdFile :cct_messageSchema}"
+  [& {:keys [sdos types] :or {sdos #{:oagi :oasis :qif}
+                              types #{:generic_messageSchema :cct_bie :generic_xsdFile :cct_messageSchema :oagis_bod}}}]
+  (alter-var-root #'sought-sdo?  (fn [_] (fn [x] (sdos x))))
+  (alter-var-root #'sought-type? (fn [_] (fn [x] (types x))))
+  (d/q '[:find [?name ...]
+         :where
+         [?e :schema_name ?name]
+         [?e :schema_sdo  ?sdo]
+         [?e :schema_type ?type]
+         [(message-data.core/sought-sdo? ?sdo)]
+         [(message-data.core/sought-type? ?type)]]
+       @(connect-atm :schema)))
 
-(defn tryme []
-  (let [bie (get-schema "urn:oagi-10.:elena.2023-07-02.ProcessInvoice-BC_1_v2")]
-    (db-for! (vector bie))))
+(defn get-attr
+  "Get all values of the argument DB attribute."
+  [attr]
+  (d/q '[:find [?val ...]
+         :in $ ?attr
+         :where [?s ?attr ?val]]
+       @(connect-atm :schema)
+       attr))
 
+(defn random-names
+  "Return a vector of n random schema names."
+  [n]
+  (let [schemas (get-schema-names)
+        len (count schemas)
+        result (atom [])]
+    (dotimes [_i n] (swap! result conj (nth schemas (rand-int len))))
+    @result))
+
+;;; -------------------- Shape ------------------------------
+#_(defn parent-child [db]
+  (d/q '[:find ?parent ?child
+         :keys parent child
+         :where
+         [?x     :element_ref         ?parent]
+         [?x     :element_complexType ?cplx1]
+         [?cplx1 :model_sequence      ?def]
+         [?def   :model_elementDef    ?cplx2]
+         [?cplx2 :element_ref         ?child]]
+       @db))
 
 (defn parent-child [db]
   (d/q '[:find ?parent ?child
@@ -46,7 +83,43 @@
          [?cplx1 :model_sequence      ?def]
          [?def   :model_elementDef    ?cplx2]
          [?cplx2 :element_name        ?child]]
-         db))
+       @db))
+
+(defn roots [db]
+  (d/q '[:find ?name
+         :where
+         [?c :schema_content   ?e]
+         [?e :model_elementDef ?d]
+         [?d :element_name     ?name]]
+       @db))
+
+(defn shape
+  "Return the shape map for the schema information provided."
+  [root pc-data]
+    (letfn [(children [p] (->> pc-data (filter #(= (:parent %) p)) (map :child)))
+            (shp [parent]
+              (reduce (fn [pmap c]
+                        (update pmap parent #(assoc % c (or (not-empty (-> (shp c) (get c))) "<data>"))))
+                      {}
+                      (children parent)))]
+      (shp root)))
+
+(defn get-shape
+  "Return the shape give a schema_name."
+  [sname]
+  (let [obj (get-schema sname)
+        db (db-for! (vector obj))
+        root (-> db roots first first)
+        pc (parent-child db)]
+    (reset! diag {:root root :pc pc :db db})
+    (shape root pc)))
+
+(defn random-shapes
+  "Return a vector of n random schema shapes."
+  [n]
+  (reduce (fn [m name] (assoc m name (get-shape name)))
+          {}
+          (random-names n)))
 
 ;;; Schema DB connection ============================================
 (defn init-db
